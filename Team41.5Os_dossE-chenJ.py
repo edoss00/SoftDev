3b1ea50d209adae51444985c305fc3a7f2d7425a
edoss00 <edoss00@stuy.edu>
2019-09-11 20:53:25 -0400

    merged with partner

---
+++ Team41.5Os_dossE-chenJ.py
@@ -0,0 +1,22 @@
+import random
+
+KREWES = {
+    'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+    'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+    'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+}
+
+x = random.randint(0,2)
+print(x)
+
+#print(len(KREWES.get('endymion')))
+
+if (x == 0):
+    randS = random.randint(0,26)
+    print(KREWES.get('orpheus')[randS])
+elif (x == 0):
+    randS = random.randint(0,22)
+    print(KREWES.get('rex')[randS])
+else:
+    randS = random.randint(0,25)
+    print(KREWES.get('endymion')[randS])
