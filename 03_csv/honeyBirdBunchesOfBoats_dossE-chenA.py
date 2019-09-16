d679733754cffbbfc6536173b66250d1d8cef8a8
edoss00 <edoss00@stuy.edu>
2019-09-16 14:37:54 -0400

    added iterator in function

--- 03_csv/honeyBirdBunchesOfBoats_dossE-chenA.py
+++ 03_csv/honeyBirdBunchesOfBoats_dossE-chenA.py
@@ -1,3 +1,5 @@
+import random
+
 #opening file
 f = open("occupations.csv","r")
 
@@ -14,7 +16,23 @@ for s in fList:
 
 #added key value pairs to dictionary
 occupations = {}
-for a in fNewList[1:-2]:
+fNewList = fNewList[1:-2]
+for a in fNewList:
     occupations[float(a[1])] = a[0]
 
 print(occupations)
+
+#generate random number
+randNum = random.randint(0,998) / 10.0
+print(randNum)
+
+#iterate and subtract through list until number is less than or equal to 0
+iterator = 0
+subtracter = randNum
+while (subtracter > 0):
+    item = float(fNewList[iterator][1])
+    subtracter = subtracter - item
+    iterator += 1
+
+#print result
+print(fNewList[iterator-1][0])
