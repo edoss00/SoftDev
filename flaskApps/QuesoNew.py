4681a5ace7f98daed1d99f163f00f36c3e68ae60
Elizabeth Doss <edoss00@stuy.edu>
2019-09-18 08:43:50 -0400

    added flask tests

--- flaskApps/QuesoNew.py
+++ flaskApps/QuesoNew.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__) #create instance of class Flask
 
 
-@app.route("/") #assign following fxn to run when root route requested
+@app.route("/flaskApps") #assign following fxn to run when root route requested
 
 def hello_world():
     print(__name__) #where will this go?
