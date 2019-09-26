be16a8123578e9c2b34cf7b9ee650ccfe8b5a439
edoss00 <edoss00@stuy.edu>
2019-09-26 17:39:34 -0400

    finished requirements

---
+++ 12_form/app.py
@@ -0,0 +1,34 @@
+#Elizabeth Doss
+#SoftDev1 pd1
+#K12 -- ECHO Echo echo
+#2019-09-26
+
+#import necessary packages
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+#initial page contains text box to enter username
+@app.route("/")
+def hello_world():
+    print(__name__)
+    #print(request)
+    return render_template("form.html") #uses template form.html
+
+#follows app route upon pressing submit
+@app.route("/auth")
+def authPage():
+    username = request.args["username"] #stores inputted username
+    request_method = request.method #stores the method by which requests are made (always GET)
+    #print statements to test out request methods
+    #print(request.args)
+    #print(request.method)
+    #print(request)
+    #print(request.headers)
+    return render_template("response.html", #uses template response.html
+                            username=username,
+                            request_method=request_method
+                            )
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
