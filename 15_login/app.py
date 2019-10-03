27558d46307f2fffec4f95f53e04d2d5d67133a8
edoss00 <edoss00@stuy.edu>
2019-10-03 19:13:32 -0400

    fixed with session

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,52 +1,63 @@
 #Elizabeth Doss
 #SoftDev1 pd1
 #K15 -- ?
-#2019-10-02
+#2019-10-03
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
 head = """
 Emory Walsh & Elizabeth Doss -- Flower Power
 SoftDev1 pd1
 K15 -- ?
-2019-10-02
+2019-10-03
 """
 
-@app.route("/")
-def form():
-    print("whyyy")
-    return render_template("temp.html",
-                               header = head)
+#random code sequence
+app.secret_key="asdfghjkl"
+
+#built in username and password
 userN = "mykolyk"
 passW = "?"
 
-user = []
+#homepage where prompted with log in
+@app.route("/")
+def start():
+    #print("whyyy")
+    return render_template("temp.html",
+                           header = head)
 
+#redirects to login page
 @app.route("/auth")
 def authenticate():
-    print("whyyyyyyy")
-    print(url_for('form'))
-    print(url_for('authenticate'))
+    #print(url_for('form'))
+    #print(url_for('authenticate'))
     #print(user)
-    user = []
-    return redirect(url_for("form"))
+    if ("user" in session):
+        return redirect(url_for("start"))
 
+#log out page after successful log in
+@app.route("/loggedout")
+def logout():
+    #print("wowow")
+    return render_template("logOut.html")
+
+#login - contains three possible routes
 @app.route("/disp_login")
 def login():
-    user.append(request.args['username'])
-    user.append(request.args['password'])
-    print(user)
-    if (user[0] == userN and user[1] == passW):
+    session['user'] = request.args['username']
+    session['pass'] =request.args['password']
+    #if accurate
+    if (userN == session['user'] and passW == session['pass']):
         return render_template("authTemp.html",
                         username = request.args['username'],
                         password = request.args['password'])
+    #if username incorrect
+    elif (userN != session['user']):
+        return render_template("errorUser.html")
+    #if password incorrect
     else:
-        user.pop()
-        user.pop()
-        return render_template("authTempError.html",
-                                username = request.args['username'],
-                                password = request.args['password'])
+        return render_template("errorPass.html")
 
 
 if __name__ == "__main__":
