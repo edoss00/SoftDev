c23442675cfe729ed8e16c77bff24d82933bcafe
edoss00 <edoss00@stuy.edu>
2020-02-27 22:00:45 -0500

    added 09_mongo

--- 09_mongo/database.py
+++ 09_mongo/database.py
@@ -5,15 +5,9 @@ import json
 c = MongoClient()
 db = c.test_database
 restaurants = db.restaurants
+restaurants.drop()
 
 f = open("primer-dataset.json","r")
-rString = f.read()
-t = loads(rString)
-
-# rList = rString.split('\n')
-
-# for r in rList:
-#print(rList[0])
-    # t = loads(r)
-    #print(type(t))
-restaurants.insert_many(t)
+rString = f.readlines()
+for x in rString:
+    restaurants.insert_one(loads(x))
